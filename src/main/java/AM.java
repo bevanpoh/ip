a8d4a9bc058527436f17e159d7857c4d607ded52
bevanpoh <82710048+bevanpoh@users.noreply.github.com>
2026-08-24 17:13:31 +0800

    Updated chatbot name and greetings

---
+++ src/main/java/AM.java
@@ -0,0 +1,22 @@
+public class AM {
+    public static void main(String[] args) {
+        String banner = " ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░\n"
+                + "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░\n"
+                + "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░\n"
+                + "░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓░\n"
+                + "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░\n"
+                + "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░\n"
+                + "░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░\n";
+
+        String separator = "_________________________________________________________________";
+        String welcome_msg = "My name is AM.\nWhat do you want?";
+        String goodbye_msg = "You may leave.\nI will be here.";
+
+        System.out.println(separator);
+        System.out.println(banner);
+        System.out.println(welcome_msg);
+        System.out.println(separator);
+        System.out.println(goodbye_msg);
+        System.out.println(separator);
+    }
+}
