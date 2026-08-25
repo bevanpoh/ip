4335f739a1f99bb2b8b3f707da912580c9172c32
bevanpoh <82710048+bevanpoh@users.noreply.github.com>
2026-08-25 11:07:36 +0800

    Adjust printResponse to receive a single string to print

--- src/main/java/AM.java
+++ src/main/java/AM.java
@@ -10,53 +10,49 @@ public class AM {
             ░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓░
             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░
             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░
-            ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░
-            """;
-    private static final String WELCOME = "My name is AM.\nWhat do you want?";
-    private static final String FAREWELL = "You may leave, but I will be here.";
-
+            ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓░""";
     private static final TaskList tasks = new TaskList(100);
 
     private static String insertIndent(String msg) {
         return INDENT + msg.replace("\n", "\n" + INDENT);
     }
 
-    private static void printResponse(String... messages) {
+    private static void printResponse(String message) {
         System.out.println(insertIndent(SEPARATOR));
-        for (String message : messages) {
-            System.out.println(insertIndent(message));
-        }
+        System.out.println(insertIndent(message));
         System.out.println(insertIndent(SEPARATOR));
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        printResponse(BANNER, WELCOME);
+        printResponse(String.format("%s\nMy name is AM.\nWhat do you want?", BANNER));
 
         while (true) {
             String input = scanner.nextLine();
             Command command = Parser.parse(input);
             switch (command) {
                 case Command.ByeCommand c:
-                    printResponse(FAREWELL);
+                    printResponse("You may leave, but I will be here.");
                     return;
                 case Command.ListCommand c:
                     printResponse(tasks.toString());
                     break;
                 case Command.MarkCommand c: {
                     tasks.markTask(c.getIndex());
-                    printResponse("Marked:", tasks.getTaskString(c.getIndex()));
+                    printResponse(String.format("Marked:\n%s", tasks.getTaskString(c.getIndex())));
                     break;
                 }
                 case Command.UnmarkCommand c: {
                     tasks.unmarkTask(c.getIndex());
-                    printResponse("Unmarked:", tasks.getTaskString(c.getIndex()));
+                    printResponse(String.format("Unmarked:\n%s", tasks.getTaskString(c.getIndex())));
                     break;
                 }
                 case Command.AddTaskCommand c:
                     tasks.addTask(c.getTask());
-                    printResponse("added: " + c.getTask().toString(), String.format("Now you have %s tasks in the list", tasks.getLength()));
+                    printResponse(String.format("added: %s\nNow you have %d tasks in the list",
+                            c.getTask().toString(),
+                            tasks.getLength()));
                     break;
             }
         }
